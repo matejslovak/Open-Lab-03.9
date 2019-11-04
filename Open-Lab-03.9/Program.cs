f2433f02bf55699097c86a9f2df63f5622c2b43d
matejslovak <joggurtik1@gmail.com>
2019-11-04 10:10:12 +0100

    Open_Lab_03_09

---
+++ Open-Lab-03.9/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Open_Lab_03_09
+{
+    class Program
+    {
+        public static bool IsPrimeNumber(int num)
+        {
+            for (int i = 2; i < num; i++)
+                if (num % i == 0)
+                {
+                    return false;
+                }
+                    return true;
+        }
+        static void Main(string[] args)
+        {
+                Console.WriteLine("Enter number:");
+                int num = int.Parse(Console.ReadLine());
+                Console.WriteLine(IsPrimeNumber(num));
+        }
+    }
+}
